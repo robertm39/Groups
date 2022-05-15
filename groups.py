b9840658c9bb7a8cfc4933cf99ac5e3df6e8fcd7
robertm39 <robertpmorton39@gmail.com>
2022-05-14 20:03:58 -0400

    group def working

--- groups.py
+++ groups.py
@@ -40,7 +40,7 @@ class Element:
         if not self in _add_group:
             raise ValueError('Right multiplicand: {}'.format(self))
         
-        result = _add_group.id
+        result = _add_group.identity
         
         for _ in range(o):
             result = result + self
@@ -61,41 +61,90 @@ class Element:
             result = result * self
         
         return result
+    
+    def __eq__(self, o):
+        return self.val == o.val
+    
+    def __neq__(self, o):
+        return self.val != o.val
+    
+    def __hash__(self):
+        return hash(self.val) * 31
+    
+    def __str__(self):
+        return str(self.val)
+    
+    def __repr__(self):
+        return 'Element({})'.format(repr(self.val))
 
 class BoundGroup:
     def __init__(self, group, op_type):
         self.group = group
         self.op_type = op_type
-
-class Group:
-    def __init__(self, elements, op, default_type):
-        self.elements = list(elements)
-        self.op = op
-        self.default_type = default_type
     
     def __enter__(self):
         global _add_group
         global _mul_group
         
-        if self.default_type == GroupType.ADDITIVE:
-            _add_group = self
-        elif self.default_type == GroupType.MULTIPLICATIVE:
-            _mul_group = self
+        if self.op_type == GroupType.ADDITIVE:
+            _add_group = self.group
+        elif self.op_type == GroupType.MULTIPLICATIVE:
+            _mul_group = self.group
         else:
-            raise ValueError('Op type: {}'.format(self.default_type))
+            raise ValueError('Op type: {}'.format(self.op_type))
             
-        _current_groups.append(BoundGroup(self, self.default_type))
+        _current_groups.append(self)
     
-    def __exit__(self):
-        popped = _current_groups.pop()
-        if not popped is self:
-            raise ValueError('Stack error: {} != {}'.format(self, popped))
+    def __exit__(self, *vals):
+        global _add_group
+        global _mul_group
+        
+        _current_groups.pop()
+        #popped = _current_groups.pop()
+        ##if popped != self:
+        #    raise ValueError('Stack error: {} != {}'.format(self, popped))
         
         if _current_groups:
             new_top = _current_groups[-1]
             if new_top.op_type == GroupType.ADDITIVE:
-                _add_group = new_top
+                _add_group = new_top.group
             elif new_top.op_type == GroupType.MULTIPLICATIVE:
-                _mul_group = new_top
+                _mul_group = new_top.group
             else:
                 raise ValueError('Op type: {}'.format(new_top.op_type))
+
+class Group:
+    def __init__(self, elements, op, default_type, identity=None):
+        self.elements = list(elements)
+        self.op = op
+        self.default_type = default_type
+        
+        # Determine the identity element if not specified
+        if identity is None:
+            for e in elements:
+                if op(e, e) == e:
+                    self.identity = e
+                    break
+            # We couldn't fine one
+            if identity is None:
+                raise ValueError('No identity element')
+        else:
+            self.identity = identity
+    
+    def add(self):
+        return BoundGroup(self, GroupType.ADDITIVE)
+    
+    def mul(self):
+        return BoundGroup(self, GroupType.MULTIPLICATIVE)
+    
+    def __contains__(self, e):
+        return e in self.elements
+    
+    def __iter__(self):
+        return iter(self.elements)
+    
+    def __enter__(self):
+        BoundGroup(self, self.default_type).__enter__()
+    
+    def __exit__(self, *vals):
+        BoundGroup(self, self.default_type).__exit__()
--- groups_test.py
+++ groups_test.py
@@ -5,3 +5,30 @@ Created on Sat May 14 19:40:57 2022
 @author: rober
 """
 
+import groups
+from groups import GroupType, Group, Element
+
+def test_1():
+    elements = Element(0), Element(1)
+    a0, a1 = elements
+    
+    op = lambda a, b: Element((a.val + b.val)%2)
+    group = Group(elements, op, GroupType.ADDITIVE, a0)
+    
+    with group:
+        print(a0 + a0)
+        print(a1 + a0)
+        print(a0 + a1)
+        print(a1 + a1)
+        print('')
+        print(0*a1)
+        print(1*a1)
+        print(2*a1)
+        print(3*a1)
+    
+    
+def main():
+    test_1()
+
+if __name__ == '__main__':
+    main()
